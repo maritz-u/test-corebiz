da9a6027ae845d94f1ad437f64474cbefa027008
Maritzu Zúñiga <maritzudelosangeles@gmail.com>
2021-06-04 16:51:48 -0400

    delete history folder

--- .history/src/data_20210604153620.js
+++
@@ -1,8 +0,0 @@
-fetch("https://5d8cdb5a443e3400143b4bea.mockapi.io/corebizchile/products").then(
-  (response) => {
-    return response.json();
-  }
-);
-//   .then((products) => {
-//     console.log(products);
-//   });
--- .history/src/data_20210604154232.js
+++
@@ -1,6 +0,0 @@
-fetch("https://5d8cdb5a443e3400143b4bea.mockapi.io/corebizchile/products").then(
-  (response) => console.log(response)
-);
-//   .then((products) => {
-//     console.log(products);
-//   });
